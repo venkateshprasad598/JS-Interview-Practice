08e4c2fca6560bb07180ee382cad7bc7bfae7435
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-08-27 08:18:21 +0530

    Add

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const users = [
-  { name: "Venkatesh", last: "Prasad", age: 23 },
-  { name: "Elon", last: "Musk", age: 25 },
-  { name: "Jeff", last: "Bezoz", age: 27 },
-];
-// console.log(users.map((data) => data.name + " " + data.last));
-// console.log(users.filter((data) => data.age === 23).map((data) => data.age));
-const ok = users.filter((data) => {
-  return data.age < 26;
-});
-console.log(ok.map((data) => data.name));
+const weekDays = (x) => {
+  switch (x) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      return "Weekday";
+      break;
+    case 6:
+      return "Weekend";
+    default:
+      return "Holiday";
+  }
+};
+const day = weekDays(8);
+console.log(day);
