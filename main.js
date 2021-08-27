cab384a24b699de6092f77f118d700bc143567e9
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-08-27 09:43:18 +0530

    Add

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
-let one = document.getElementById("one");
-console.log(one);
+const myName = [
+  { name: "v", id: 1 },
+  { name: "e", id: 2 },
+  { name: "n", id: 3 },
+];
 
-let div = document.createElement("div");
-div.innerHTML = "<h1>Hello</h1>";
-document.body.append(div);
-console.log(document);
+function getName() {
+  setTimeout(() => {
+    let output = "";
+    for (let i = 0; i < myName.length; i++) {
+      output = myName[i].name;
+      console.log(output);
+    }
+  }, 1000);
+}
+
+// function addName(x, callback) {
+//   setTimeout(() => {
+//     myName.push(x);
+//     callback();
+//   }, 2000);
+// }
+// addName({ name: "a", id: 4 }, getName);
+
+const addName = (x) => {
+  return new Promise((resolve, rejeect) => {
+    setTimeout(() => {
+      myName.push(x);
+      let condition = true;
+      switch (condition) {
+        case true:
+          resolve();
+          break;
+        case false:
+          rejeect("error");
+      }
+    }, 2000);
+  });
+};
+
+// addName({ name: "a", id: 4 })
+//   .then(getName)
+//   .catch((error) => console.log(error));
+
+async function asyncFunc() {
+  await addName({ name: "a", id: 4 });
+  getName();
+}
+asyncFunc();
