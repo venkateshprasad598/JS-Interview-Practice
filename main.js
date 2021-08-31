4ca574832cf76da9206bbe9cf3d4f41f848917d6
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-08-31 10:51:20 +0530

    Add

--- main.js
+++ main.js
@@ -1,50 +1,53 @@
-places = [
-  { name: "Paris", id: 1 },
-  { name: "London", id: 2 },
-  { name: "Barcelona", id: 3 },
-];
+// let newPerson = new Person("Venkatesh", "Prasad");
+// console.log(newPerson.first);
 
-function getName() {
-  setTimeout(() => {
-    let output = "";
-    places.map((data) => {
-      output = data.name;
-      console.log(output);
-    });
-  }, 1000);
-}
+// places = [
+//   { name: "Paris", id: 1 },
+//   { name: "London", id: 2 },
+//   { name: "Barcelona", id: 3 },
+// ];
 
-// function addName(add, callback) {
+// function getName() {
 //   setTimeout(() => {
-//     places.push(add);
-//     callback();
-//   }, 2000);
+//     let output = "";
+//     places.map((data) => {
+//       output = data.name;
+//       console.log(output);
+//     });
+//   }, 1000);
 // }
-// addName({ name: "Italy", id: 4 }, getName);
 
-function addName(add) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      places.push(add);
-      let condition = true;
-      if (condition) {
-        resolve();
-      } else {
-        reject(console.log("Error"));
-      }
-    }, 2000);
-  });
-}
-// addName({ name: "Italy", id: 4 })
-//   .then(() => {
-//     getName();
-//   })
-//   .catch((error) => {
-//     return error;
+// // function addName(add, callback) {
+// //   setTimeout(() => {
+// //     places.push(add);
+// //     callback();
+// //   }, 2000);
+// // }
+// // addName({ name: "Italy", id: 4 }, getName);
+
+// function addName(add) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       places.push(add);
+//       let condition = true;
+//       if (condition) {
+//         resolve();
+//       } else {
+//         reject(console.log("Error"));
+//       }
+//     }, 2000);
 //   });
-async function asyncFunc() {
-  await addName({ name: "Italy", id: 4 });
-  getName();
-}
+// }
+// // addName({ name: "Italy", id: 4 })
+// //   .then(() => {
+// //     getName();
+// //   })
+// //   .catch((error) => {
+// //     return error;
+// //   });
+// async function asyncFunc() {
+//   await addName({ name: "Italy", id: 4 });
+//   getName();
+// }
 
-asyncFunc();
+// asyncFunc();
